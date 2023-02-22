f3ca6bbe0cb3a77c2c20ae55ffe65f34e17fb531
Ardaq Saduaqas <ardaqs@gmail.com>
2023-02-22 09:08:35 +0600

    Cyrl and Latn

---
+++ src/cyrl.ts
@@ -0,0 +1,296 @@
+export const Cyrl = [
+  {
+    num: 1,
+    upper: { c: 'А', code: 0x0410, name: 'Cyrillic capital letter A' },
+    lower: { c: 'а', code: 0x0430, name: 'Cyrillic small letter A' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 2,
+    upper: { c: 'Ә', code: 0x04D8, name: 'Cyrillic capital letter Schwa' },
+    lower: { c: 'ә', code: 0x04D9, name: 'Cyrillic small letter Schwa' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 3,
+    upper: { c: 'Б', code: 0x0411, name: 'Cyrillic capital letter Be' },
+    lower: { c: 'б', code: 0x0431, name: 'Cyrillic small letter Be' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 4,
+    upper: { c: 'В', code: 0x0412, name: 'Cyrillic capital letter Ve' },
+    lower: { c: 'в', code: 0x0432, name: 'Cyrillic small letter Ve' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 5,
+    upper: { c: 'Г', code: 0x0413, name: 'Cyrillic capital letter Ghe' },
+    lower: { c: 'г', code: 0x0433, name: 'Cyrillic small letter Ghe' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 6,
+    upper: { c: 'Ғ', code: 0x0492, name: 'Cyrillic capital letter Ghe with stroke' },
+    lower: { c: 'ғ', code: 0x0493, name: 'Cyrillic small letter Ghe with stroke' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 7,
+    upper: { c: 'Д', code: 0x0414, name: 'Cyrillic capital letter De' },
+    lower: { c: 'д', code: 0x0434, name: 'Cyrillic small letter De' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 8,
+    upper: { c: 'Е', code: 0x0415, name: 'Cyrillic capital letter Ie' },
+    lower: { c: 'е', code: 0x0435, name: 'Cyrillic small letter Ie' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 9,
+    upper: { c: 'Ё', code: 0x0401, name: 'Cyrillic capital letter Io' },
+    lower: { c: 'ё', code: 0x0451, name: 'Cyrillic small letter Io' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 10,
+    upper: { c: 'Ж', code: 0x0416, name: 'Cyrillic capital letter Zhe' },
+    lower: { c: 'ж', code: 0x0436, name: 'Cyrillic small letter Zhe' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 11,
+    upper: { c: 'З', code: 0x0417, name: 'Cyrillic capital letter Ze' },
+    lower: { c: 'з', code: 0x0437, name: 'Cyrillic small letter Ze' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 12,
+    upper: { c: 'И', code: 0x0418, name: 'Cyrillic capital letter I' },
+    lower: { c: 'и', code: 0x0438, name: 'Cyrillic small letter I' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 13,
+    upper: { c: 'Й', code: 0x0419, name: 'Cyrillic capital letter Short I' },
+    lower: { c: 'й', code: 0x0439, name: 'Cyrillic small letter Short I' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 14,
+    upper: { c: 'К', code: 0x041A, name: 'Cyrillic capital letter Ka' },
+    lower: { c: 'к', code: 0x043A, name: 'Cyrillic small letter Ka' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 15,
+    upper: { c: 'Қ', code: 0x049A, name: 'Cyrillic capital letter Ka with descender' },
+    lower: { c: 'қ', code: 0x049B, name: 'Cyrillic small letter Ka with descender' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 16,
+    upper: { c: 'Л', code: 0x041B, name: 'Cyrillic capital letter El' },
+    lower: { c: 'л', code: 0x043B, name: 'Cyrillic small letter El' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 17,
+    upper: { c: 'М', code: 0x041C, name: 'Cyrillic capital letter Em' },
+    lower: { c: 'м', code: 0x043C, name: 'Cyrillic small letter Em' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 18,
+    upper: { c: 'Н', code: 0x041D, name: 'Cyrillic capital letter En' },
+    lower: { c: 'н', code: 0x043D, name: 'Cyrillic small letter En' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 19,
+    upper: { c: 'Ң', code: 0x04A2, name: 'Cyrillic capital letter En with descender' },
+    lower: { c: 'ң', code: 0x04A3, name: 'Cyrillic small letter En with descender' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 20,
+    upper: { c: 'О', code: 0x041E, name: 'Cyrillic capital letter O' },
+    lower: { c: 'о', code: 0x043E, name: 'Cyrillic small letter O' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 21,
+    upper: { c: 'Ө', code: 0x04E8, name: 'Cyrillic capital letter Barred O' },
+    lower: { c: 'ө', code: 0x04E9, name: 'Cyrillic small letter Barred O' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 22,
+    upper: { c: 'П', code: 0x041F, name: 'Cyrillic capital letter Pe' },
+    lower: { c: 'п', code: 0x043F, name: 'Cyrillic small letter Pe' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 23,
+    upper: { c: 'Р', code: 0x0420, name: 'Cyrillic capital letter Er' },
+    lower: { c: 'р', code: 0x0440, name: 'Cyrillic small letter Er' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 24,
+    upper: { c: 'С', code: 0x0421, name: 'Cyrillic capital letter Es' },
+    lower: { c: 'с', code: 0x0441, name: 'Cyrillic small letter Es' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 25,
+    upper: { c: 'Т', code: 0x0422, name: 'Cyrillic capital letter Te' },
+    lower: { c: 'т', code: 0x0442, name: 'Cyrillic small letter Te' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 26,
+    upper: { c: 'У', code: 0x0423, name: 'Cyrillic capital letter U' },
+    lower: { c: 'у', code: 0x0443, name: 'Cyrillic small letter U' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 27,
+    upper: { c: 'Ұ', code: 0x04B0, name: 'Cyrillic capital letter Straight U with stroke' },
+    lower: { c: 'ұ', code: 0x04B1, name: 'Cyrillic small letter Straight U with stroke' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 28,
+    upper: { c: 'Ү', code: 0x04AE, name: 'Cyrillic capital letter Straight U' },
+    lower: { c: 'ү', code: 0x04AF, name: 'Cyrillic small letter Straight U' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 29,
+    upper: { c: 'Ф', code: 0x0424, name: 'Cyrillic capital letter Ef' },
+    lower: { c: 'ф', code: 0x0444, name: 'Cyrillic small letter Ef' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 30,
+    upper: { c: 'Х', code: 0x0425, name: 'Cyrillic capital letter Ha' },
+    lower: { c: 'х', code: 0x0445, name: 'Cyrillic small letter Ha' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 31,
+    upper: { c: 'Һ', code: 0x04BA, name: 'Cyrillic capital letter Shha' },
+    lower: { c: 'һ', code: 0x04BB, name: 'Cyrillic small letter Shha' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 32,
+    upper: { c: 'Ц', code: 0x0426, name: 'Cyrillic capital letter Tse' },
+    lower: { c: 'ц', code: 0x0446, name: 'Cyrillic small letter Tse' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 33,
+    upper: { c: 'Ч', code: 0x0427, name: 'Cyrillic capital letter Che' },
+    lower: { c: 'ч', code: 0x0447, name: 'Cyrillic small letter Che' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 34,
+    upper: { c: 'Ш', code: 0x0428, name: 'Cyrillic capital letter Sha' },
+    lower: { c: 'ш', code: 0x0448, name: 'Cyrillic small letter Sha' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 35,
+    upper: { c: 'Щ', code: 0x0429, name: 'Cyrillic capital letter Shcha' },
+    lower: { c: 'щ', code: 0x0449, name: 'Cyrillic small letter Shcha' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 36,
+    upper: { c: 'Ъ', code: 0x042A, name: 'Cyrillic capital letter Hard sign' },
+    lower: { c: 'ъ', code: 0x044A, name: 'Cyrillic small letter Hard sign' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 37,
+    upper: { c: 'Ы', code: 0x042B, name: 'Cyrillic capital letter Yeru' },
+    lower: { c: 'ы', code: 0x044B, name: 'Cyrillic small letter Yeru' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 38,
+    upper: { c: 'І', code: 0x0406, name: 'Cyrillic capital letter Byelorussian-Ukrainian I' },
+    lower: { c: 'і', code: 0x0456, name: 'Cyrillic small letter Byelorussian-Ukrainian I' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 39,
+    upper: { c: 'Ь', code: 0x042C, name: 'Cyrillic capital letter Soft sign' },
+    lower: { c: 'ь', code: 0x044C, name: 'Cyrillic small letter Soft sign' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 40,
+    upper: { c: 'Э', code: 0x042D, name: 'Cyrillic capital letter E' },
+    lower: { c: 'э', code: 0x044D, name: 'Cyrillic small letter E' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 41,
+    upper: { c: 'Ю', code: 0x042E, name: 'Cyrillic capital letter Yu' },
+    lower: { c: 'ю', code: 0x044E, name: 'Cyrillic small letter Yu' },
+    ipa: null,
+    tag: null
+  },
+  {
+    num: 42,
+    upper: { c: 'Я', code: 0x042F, name: 'Cyrillic capital letter Ya' },
+    lower: { c: 'я', code: 0x044F, name: 'Cyrillic small letter Ya' },
+    ipa: null,
+    tag: null
+  }
+];
---
+++ src/latn.ts
@@ -0,0 +1,244 @@
+export const Latn = [
+  {
+    num: 1,
+    upper: { c: 'A', code: 0x0041, name: 'Latin capital letter A' },
+    lower: { c: 'a', code: 0x0061, name: 'Latin small letter A' },
+    ipa: '[a]',
+    tag: { extra: false }
+  },
+  {
+    num: 2,
+    upper: { c: 'Ä', code: 0x00C4, name: 'Latin capital letter A with diaeresis' },
+    lower: { c: 'ä', code: 0x00E4, name: 'Latin small letter A with diaeresis' },
+    ipa: '[æ]',
+    tag: { extra: false }
+  },
+  {
+    num: 3,
+    upper: { c: 'B', code: 0x0042, name: 'Latin capital letter B' },
+    lower: { c: 'b', code: 0x0062, name: 'Latin small letter B' },
+    ipa: '[b]',
+    tag: { extra: false }
+  },
+  {
+    num: 4,
+    upper: { c: 'D', code: 0x0044, name: 'Latin capital letter D' },
+    lower: { c: 'd', code: 0x0064, name: 'Latin small letter D' },
+    ipa: '[d]',
+    tag: { extra: false }
+  },
+  {
+    num: 5,
+    upper: { c: 'E', code: 0x0045, name: 'Latin capital letter E' },
+    lower: { c: 'e', code: 0x0065, name: 'Latin small letter E' },
+    ipa: '[e]',
+    tag: { extra: false }
+  },
+  {
+    num: 6,
+    upper: { c: 'F', code: 0x0046, name: 'Latin capital letter F' },
+    lower: { c: 'f', code: 0x0066, name: 'Latin small letter F' },
+    ipa: '[f]',
+    tag: { extra: false }
+  },
+  {
+    num: 7,
+    upper: { c: 'G', code: 0x0047, name: 'Latin capital letter G' },
+    lower: { c: 'g', code: 0x0067, name: 'Latin small letter G' },
+    ipa: '[g]',
+    tag: { extra: false }
+  },
+  {
+    num: 8,
+    upper: { c: 'Ğ', code: 0x011E, name: 'Latin capital letter G with breve' },
+    lower: { c: 'ğ', code: 0x011F, name: 'Latin small letter G with breve' },
+    ipa: '[ʁ]',
+    tag: { extra: false }
+  },
+  {
+    num: 9,
+    upper: { c: 'H', code: 0x0048, name: 'Latin capital letter H' },
+    lower: { c: 'h', code: 0x0068, name: 'Latin small letter H' },
+    ipa: '[χ]',
+    tag: { extra: false }
+  },
+  {
+    num: 10,
+    upper: { c: 'İ', code: 0x0130, name: 'Latin capital letter I with dot above' },
+    lower: { c: 'i', code: 0x0069, name: 'Latin small letter I' },
+    ipa: '[i]',
+    tag: { extra: false }
+  },
+  {
+    num: 11,
+    upper: { c: 'I', code: 0x0049, name: 'Latin capital letter I' },
+    lower: { c: 'ı', code: 0x0131, name: 'Latin small letter Dotless I' },
+    ipa: '[ɨ]',
+    tag: { extra: false }
+  },
+  {
+    num: 12,
+    upper: { c: 'J', code: 0x004A, name: 'Latin capital letter J' },
+    lower: { c: 'j', code: 0x006A, name: 'Latin small letter J' },
+    ipa: '[ʒ]',
+    tag: { extra: false }
+  },
+  {
+    num: 13,
+    upper: { c: 'K', code: 0x004B, name: 'Latin capital letter K' },
+    lower: { c: 'k', code: 0x006B, name: 'Latin small letter K' },
+    ipa: '[k]',
+    tag: { extra: false }
+  },
+  {
+    num: 14,
+    upper: { c: 'L', code: 0x004C, name: 'Latin capital letter L' },
+    lower: { c: 'l', code: 0x006C, name: 'Latin small letter L' },
+    ipa: '[l]',
+    tag: { extra: false }
+  },
+  {
+    num: 15,
+    upper: { c: 'M', code: 0x004D, name: 'Latin capital letter M' },
+    lower: { c: 'm', code: 0x006D, name: 'Latin small letter M' },
+    ipa: '[m]',
+    tag: { extra: false }
+  },
+  {
+    num: 16,
+    upper: { c: 'N', code: 0x004E, name: 'Latin capital letter N' },
+    lower: { c: 'n', code: 0x006E, name: 'Latin small letter N' },
+    ipa: '[n]',
+    tag: { extra: false }
+  },
+  {
+    num: 17,
+    // upper: { c: 'Ŋ', code: 0x014A, name: 'Latin capital letter Eng' },
+    // lower: { c: 'ŋ', code: 0x014B, name: 'Latin small letter Eng' },
+    upper: { c: 'Ñ', code: 0x00D1, name: 'Latin capital letter N with tilde' },
+    lower: { c: 'ñ', code: 0x00F1, name: 'Latin small letter N with tilde' },
+    ipa: '[ŋ]',
+    tag: { extra: false }
+  },
+  {
+    num: 18,
+    upper: { c: 'O', code: 0x004F, name: 'Latin capital letter O' },
+    lower: { c: 'o', code: 0x006F, name: 'Latin small letter O' },
+    ipa: '[o]',
+    tag: { extra: false }
+  },
+  {
+    num: 19,
+    upper: { c: 'Ö', code: 0x00D6, name: 'Latin capital letter O with diaeresis' },
+    lower: { c: 'ö', code: 0x00F6, name: 'Latin small letter O with diaeresis' },
+    ipa: '[ɵ]',
+    tag: { extra: false }
+  },
+  {
+    num: 20,
+    upper: { c: 'P', code: 0x0050, name: 'Latin capital letter P' },
+    lower: { c: 'p', code: 0x0070, name: 'Latin small letter P' },
+    ipa: '[p]',
+    tag: { extra: false }
+  },
+  {
+    num: 21,
+    upper: { c: 'Q', code: 0x0051, name: 'Latin capital letter Q' },
+    lower: { c: 'q', code: 0x0071, name: 'Latin small letter Q' },
+    ipa: '[q]',
+    tag: { extra: false }
+  },
+  {
+    num: 22,
+    upper: { c: 'R', code: 0x0052, name: 'Latin capital letter R' },
+    lower: { c: 'r', code: 0x0072, name: 'Latin small letter R' },
+    ipa: '[r]',
+    tag: { extra: false }
+  },
+  {
+    num: 23,
+    upper: { c: 'S', code: 0x0053, name: 'Latin capital letter S' },
+    lower: { c: 's', code: 0x0073, name: 'Latin small letter S' },
+    ipa: '[s]',
+    tag: { extra: false }
+  },
+  {
+    num: 24,
+    upper: { c: 'Ş', code: 0x015E, name: 'Latin capital letter S with cedilla' },
+    lower: { c: 'ş', code: 0x015F, name: 'Latin small letter S with cedilla' },
+    ipa: '[ʃ]',
+    tag: { extra: false }
+  },
+  {
+    num: 25,
+    upper: { c: 'T', code: 0x0054, name: 'Latin capital letter T' },
+    lower: { c: 't', code: 0x0074, name: 'Latin small letter T' },
+    ipa: '[t]',
+    tag: { extra: false }
+  },
+  {
+    num: 26,
+    upper: { c: 'U', code: 0x0055, name: 'Latin capital letter U' },
+    lower: { c: 'u', code: 0x0075, name: 'Latin small letter U' },
+    ipa: '[u]',
+    tag: { extra: false }
+  },
+  {
+    num: 27,
+    upper: { c: 'Ū', code: 0x016A, name: 'Latin capital letter U with macron' },
+    lower: { c: 'ū', code: 0x016B, name: 'Latin small letter U with macron' },
+    ipa: '[ʊ]',
+    tag: { extra: false }
+  },
+  {
+    num: 28,
+    upper: { c: 'Ü', code: 0x00DC, name: 'Latin capital letter U with diaeresis' },
+    lower: { c: 'ü', code: 0x00FC, name: 'Latin small letter U with diaeresis' },
+    ipa: '[ø]',
+    tag: { extra: false }
+  },
+  {
+    num: 29,
+    upper: { c: 'V', code: 0x0056, name: 'Latin capital letter V' },
+    lower: { c: 'v', code: 0x0076, name: 'Latin small letter V' },
+    ipa: '[v]',
+    tag: { extra: false }
+  },
+  {
+    num: 30,
+    upper: { c: 'Y', code: 0x0059, name: 'Latin capital letter Y' },
+    lower: { c: 'y', code: 0x0079, name: 'Latin small letter Y' },
+    ipa: '[ɯ]',
+    tag: { extra: false }
+  },
+  {
+    num: 31,
+    upper: { c: 'Z', code: 0x005A, name: 'Latin capital letter Z' },
+    lower: { c: 'z', code: 0x007A, name: 'Latin small letter Z' },
+    ipa: '[z]',
+    tag: { extra: false }
+  },
+
+  // Extra symbols
+  {
+    num: 32,
+    upper: { c: 'C', code: 0x0043, name: 'Latin capital letter C' },
+    lower: { c: 'c', code: 0x0063, name: 'Latin small letter C' },
+    ipa: null,
+    tag: { extra: true }
+  },
+  {
+    num: 33,
+    upper: { c: 'W', code: 0x0057, name: 'Latin capital letter W' },
+    lower: { c: 'w', code: 0x0077, name: 'Latin small letter W' },
+    ipa: null,
+    tag: { extra: true }
+  },
+  {
+    num: 34,
+    upper: { c: 'X', code: 0x0058, name: 'Latin capital letter X' },
+    lower: { c: 'x', code: 0x0078, name: 'Latin small letter X' },
+    ipa: null,
+    tag: { extra: true }
+  },
+];
