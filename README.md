3a9f49107e4f87bf99032ee457707878ccb64c1c
Ardaq Saduaqas <ardaqs@gmail.com>
2023-02-24 12:44:44 +0600

    Separate transcodeText() and transcodeJson()

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # transcode-kz
+
+:kazakhstan: A library for transcoding Kazakh between Cyrillic and Latin encodings.
+
+## Exports
+
+```js
+/**
+ * Transcodes a plain text.
+ * @param text - source text.
+ * @param direction - transcoding direction ('cyr2lat' | 'lat2cyr').
+ * @returns transcoded text.
+ */
+export function transcodeText(text: string, direction: string = 'cyr2lat'): string {
+    ...
+}
+```
+
+```js
+/**
+ * Creates an object from a JSON string and transcodes only string values (deeply nested) in the object.
+ * @param json - source JSON string.
+ * @param direction - transcoding direction ('cyr2lat' | 'lat2cyr').
+ * @param indent - number of spaces used for indentation.
+ * @returns transcoded JSON string.
+ */
+export function transcodeJson(json: string, direction: string = 'cyr2lat', indent: number = 2): string {
+    ...
+}
+```
+
+## Usage examples
+
+Transcode a plain text:
+
+```js
+import { transcodeText } from 'transcode-kz';
+
+const srcText = "Қазақша мәтін";
+const dstText = transcodeText(srcText);
+```
+
+Transcode only string values in a JSON:
+
+```js
+import { transcodeJson } from 'transcode-kz';
+
+const srcJson = "{ \"city\": \"Қала\" }";
+const dstJson = transcodeJson(srcJson);
+```
+
+## License
+
+[LICENSE (MIT)](LICENSE)
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,31 @@ import { Lat } from './lat';
 import { Cyr2Lat } from './cyr2lat';
 import { Lat2Cyr } from './lat2cyr';
 
-const transcode = (s: string, direction: string = 'cyr2lat', parseJson: boolean = false, indent: number = 2) => {
-  return parseJson ? transcodeJson(s, direction, indent) : transcodeString(s, direction);
+/**
+ * Transcodes a plain text.
+ * @param text - source text.
+ * @param direction - transcoding direction ('cyr2lat' | 'lat2cyr').
+ * @returns transcoded text.
+ */
+const transcodeText = (text: string, direction: string = 'cyr2lat') => {
+  const map = direction === 'lat2cyr' ? Lat2Cyr : Cyr2Lat;
+  let res = '';
+  for (const c of text) {
+    const r = map.get(c);
+    res += !r ? c : r;
+  }
+  return res;
 }
 
-function transcodeJson(s: string, direction: string, indent: number): string {
-  const obj = JSON.parse(s);
+/**
+ * Creates an object from a JSON string and transcodes only string values (deeply nested) in the object.
+ * @param json - source JSON string.
+ * @param direction - transcoding direction ('cyr2lat' | 'lat2cyr').
+ * @param indent - number of spaces used for indentation.
+ * @returns transcoded JSON string.
+ */
+const transcodeJson = (json: string, direction: string = 'cyr2lat', indent: number = 2) => {
+  const obj = JSON.parse(json);
   transcodeObject(obj, direction); // mutation!
   return JSON.stringify(obj, null, indent);
 }
@@ -18,21 +37,11 @@ function transcodeObject(obj: any, direction: string) {
   for (const key in obj) {
     const value = obj[key];
     if (typeof value === 'string') {
-      obj[key] = transcodeString(value, direction);
+      obj[key] = transcodeText(value, direction);
     } else if (typeof value === 'object') {
       transcodeObject(value, direction);
     }
   }
 }
 
-function transcodeString(s: string, direction: string): string {
-  const map = direction === 'lat2cyr' ? Lat2Cyr : Cyr2Lat;
-  let res = '';
-  for (const c of s) {
-    const r = map.get(c);
-    res += !r ? c : r;
-  }
-  return res;
-}
-
-export { Cyr, Lat, Cyr2Lat, Lat2Cyr, transcode }
+export { Cyr, Lat, Cyr2Lat, Lat2Cyr, transcodeText, transcodeJson }
